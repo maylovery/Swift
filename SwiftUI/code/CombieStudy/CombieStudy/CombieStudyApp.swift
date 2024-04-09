01f61cd431ec64c569ff81917d713fdabb22ae19
may <ytxinguan035@163.com>
2024-04-10 05:59:12 +0800

    init Combine-UI

---
+++ SwiftUI/code/CombieStudy/CombieStudy/CombieStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombieStudyApp.swift
+//  CombieStudy
+//
+//  Created by Tao Yu on 2024/4/8.
+//
+
+import SwiftUI
+
+@main
+struct CombieStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
